cf8fc5a9ef70a636efeb6e10fe52f382b45cc2e0
Yoav Fishman <fish@wearegofer.com>
2019-01-15 16:28:15 +0200

    adding address and remove button

--- src/app/components/contacts/dataService.js
+++ src/app/components/contacts/dataService.js
@@ -1,6 +1,6 @@
 export default function DataService($http) {
   function getContacts() {
-    return $http.get('https://randomuser.me/api/?inc=name,phone,picture&results=10').then(response => {
+    return $http.get('https://randomuser.me/api/?inc=name,phone,picture,location&results=10').then(response => {
       return response.data.results;
     });
   }
