872e7937df8c0e61e3fb322c05e6a7d0557a18fa
Yoav Fishman <fish@wearegofer.com>
2019-01-15 14:58:09 +0200

    data service, resolving contacts with ui.router resolve

--- src/app/app.js
+++ src/app/app.js
@@ -1,22 +1,30 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router'
+import ContactsController from './components/contacts/contactsController'
+import dataService from './components/contacts/dataService'
 import '../style/app.css';
 
 const MODULE_NAME = 'app';
 
 const app = angular.module(MODULE_NAME, [uiRouter]);
-  
+app.factory('dataService', dataService)
 app.config(($locationProvider, $urlRouterProvider, $stateProvider ) => {
     // $urlRouterProvider.other('/');
 
     $stateProvider
       .state('contacts', {
           url: '/',
-          template: require('../contacts/index.html')
+          template: require('./components/contacts/index.html'),
+          controller: ContactsController,
+          resolve:{
+              _contacts: function(dataService){
+                return dataService.getContacts();
+            }
+          }
       })
       .state('another',{
         url: '/another',
-        template: require('../another/index.html')
+        template: require('./components/another/index.html')
       })
 
       $locationProvider.html5Mode(true)
