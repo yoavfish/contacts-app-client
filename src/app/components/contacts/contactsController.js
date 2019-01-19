6ad136ece3480aa91778998ea39f293da7366b90
Yoav Fishman <fish@wearegofer.com>
2019-01-19 17:51:29 +0200

    order in contacts controller

--- src/app/components/contacts/contactsController.js
+++ src/app/components/contacts/contactsController.js
@@ -6,13 +6,13 @@ export default ($scope, _contacts, dataService) => {
       currentPage: Math.ceil( data.offset / data.limit) 
     }
   }
+  $scope.pagination = []
   $scope.updatePagination(_contacts)
+
   $scope.sort = {
     sortType: 'name.first',
     sortDirection: 1
   }
-
-  $scope.pagination = []
   $scope.contacts = _contacts.docs
   $scope.showAddContactRow = false
   $scope.newContact = null
@@ -24,13 +24,26 @@ export default ($scope, _contacts, dataService) => {
     cell: null,
     picture: null,
   }
+  
+  $scope.setShowAddContactRow = (value) => {
+    $scope.resetNewContact()
+    $scope.showAddContactRow = value
+  }
+
+  $scope.setEditContactId = (contactId) => {
+    $scope.editContactId = contactId
+    if(!$scope.editContactId) $scope.search($scope.pagination.currentPage)
+  }
+
+  $scope.showEditRow = (contactId) => $scope.editContactId === contactId
+
+  $scope.resetNewContact = () => $scope.newContact = angular.copy($scope.contactModel)
 
   $scope.saveContact = () => {
     dataService.saveContact($scope.newContact)
     .then(
-      data => {
-      const newContact = angular.copy(data)
-      $scope.contacts.push(newContact)
+      () => {
+      $scope.search($scope.pagination.currentPage)
       $scope.resetNewContact()
     },
       error => console.error('Error saving new contact', error)
@@ -55,20 +68,6 @@ export default ($scope, _contacts, dataService) => {
       error => console.error(`Error updating contact (id: ${contactId})`, error)
     )
   }
-  
-  $scope.setShowAddContactRow = (value) => {
-    $scope.resetNewContact()
-    $scope.showAddContactRow = value
-  }
-
-  $scope.setEditContactId = (contactId) => {
-    $scope.editContactId = contactId
-    if(!$scope.editContactId) $scope.search($scope.pagination.currentPage)
-  }
-
-  $scope.showEditRow = (contactId) => $scope.editContactId === contactId
-
-  $scope.resetNewContact = () => $scope.newContact = angular.copy($scope.contactModel)
 
   $scope.search = (page = 0, sortType = '', sortDirection = '') => {
     $scope.sort.sortType = sortType
